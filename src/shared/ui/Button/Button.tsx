757a46241f2ce19294f598bdc549e7ef6ec14480
RuMax21 <cherednikov.maxim@mail.ru>
2026-02-24 15:38:15 +0300

    refactor: migrate Button styles to module

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,9 +1,9 @@
 import type { ButtonProps } from './Button.types';
-import './Button.scss';
+import styles from './Button.module.scss';
 
 const Button: React.FC<ButtonProps> = ({ onClick, children, disabled = false, className = '' }) => {
   return (
-    <button onClick={onClick} disabled={disabled} className={`button ${className}`}>
+    <button onClick={onClick} disabled={disabled} className={`${styles.button} ${className}`}>
       {children}
     </button>
   );
